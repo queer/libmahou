0db71125a7a4a490ec5048be199bf79d6ca07aa0
amy null <null@amy.gg>
2021-01-27 09:36:57 -0500

    Basic msg struct

--- lib/mahou/message.ex
+++ lib/mahou/message.ex
@@ -1,3 +1,10 @@
 defmodule Mahou.Message do
-  #
+  use TypedStruct
+  alias Mahou.Message.{ChangeContainerStatus, CreateContainer}
+
+  typedstruct do
+    field :ts, non_neg_integer()
+    field :msg, ChangeContainerStatus.t()
+                | CreateContainer.t()
+  end
 end
