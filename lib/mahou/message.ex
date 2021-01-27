5d8557e734b3c31f61ecf46cd6b4fd365d685434
amy null <null@amy.gg>
2021-01-27 09:37:58 -0500

    Create helper

--- lib/mahou/message.ex
+++ lib/mahou/message.ex
@@ -4,7 +4,14 @@ defmodule Mahou.Message do
 
   typedstruct do
     field :ts, non_neg_integer()
-    field :msg, ChangeContainerStatus.t()
-                | CreateContainer.t()
+    field :payload, ChangeContainerStatus.t()
+                    | CreateContainer.t()
+  end
+
+  def create(payload) do
+    %__MODULE__{
+      ts: :os.system_time(:millisecond),
+      payload: payload,
+    }
   end
 end
