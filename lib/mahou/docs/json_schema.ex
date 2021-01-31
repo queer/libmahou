95af75813249fc97844fd561216e449ff68d6302
amy null <null@amy.gg>
2021-01-31 10:44:45 -0500

    Don't force into json strings

--- lib/mahou/docs/json_schema.ex
+++ lib/mahou/docs/json_schema.ex
@@ -1,6 +1,10 @@
 defmodule Mahou.Docs.JsonSchema do
   def of(module, type \\ :t) when is_atom(module) do
-    json = GenJsonSchema.gen module, type
+    json =
+      module
+      |> GenJsonSchema.gen(type)
+      |> Jason.decode!
+
     # Will raise if invalid schema
     ExJsonSchema.Schema.resolve json
     {:ok, json}
